8958dd786dddb648133f6d5ea33af680d0ef66af
Caio Rocha <caiorochauba@outlook.com>
2022-04-30 12:14:35 -0300

    commit

--- pasta.js/project.js
+++ pasta.js/project.js
@@ -7,19 +7,25 @@ const superTrunfo = { // criando objeto (o ptojeto))
 const tabelaDeJogos = {
     imagem: "<img class='img' src='/pasta.img/tabelaimg.jpeg'/>",
     text: "<p class='paragrafo'> " + "Tabela de Jogo:" + "<br>" + "feito na ImersãoDev Alura." + "<br>" + "(HTML,CSS,JavaScript)." + "</p>",
-    link: "#"
+    
 
 }
 const calculadorIMC = {
     imagem: "<img class='img' src='/pasta.img/calculadoraIMC.jpeg'/>",
     text: "<p class='paragrafo'> " + "calcule seu IMC:" + "<br>" + "projeto pessoal." + "<br>" + "(HTML,CSS,JavaScript)." + "</p>",
-    link: "#"
+    
 
 }
 const mentalista = {
     imagem: "<img class='img' src='/pasta.img/imgProjeto2.jpeg'/>",
     text: "<p class='paragrafo'> " + "joguinho Mentalista:" + "<br>" + "feito na imersão Dev alura." + "<br>" + "(HTML,CSS,JavaScript)." + "</p>",
-    link: "#"
+    
+
+}
+const ladingPage = {
+    imagem: "<img class='img' src='/pasta.img/ladingPage.jpeg'/>",
+    text: "<p class='paragrafo'> " + "Lading Page Vendas:" + "<br>" + "Projeto pessoal." + "<br>" + "(HTML,CSS,JavaScript)." + "</p>",
+    
 
 }
 //arry de projetos
@@ -29,20 +35,21 @@ const projetos = [superTrunfo, tabelaDeJogos, calculadorIMC,mentalista]
 const liProjetoSp = " <li class='sPTrunfo'> ";
 
 
-function lego(imagem, descricao, link) { // função recebendo a img, descrição e o link do projeto
+function lego(imagem, descricao) { // função recebendo a img, descrição e o link do projeto
 
     const elementoProjeto = document.getElementById("projetos"); // identificando a minha div do html com id
 
-    const montado = liProjetoSp + imagem + descricao + link +"</li>"; // montando o projeto 
+    const montado = liProjetoSp + imagem + descricao +"</li>"; // montando o projeto 
 
     elementoProjeto.innerHTML = elementoProjeto.innerHTML + montado; // adicionando projeto montado no html
 
 
 };
 // adicionando os elementos no parametro da função
-lego(superTrunfo.imagem, superTrunfo.text, superTrunfo.link);
+lego(superTrunfo.imagem, superTrunfo.text);
 lego(tabelaDeJogos.imagem, tabelaDeJogos.text);
 lego(calculadorIMC.imagem, calculadorIMC.text);
 lego(mentalista.imagem, mentalista.text);
+lego(ladingPage.imagem, ladingPage.text);
 
 
